dda6652b50d569a0c710d67d5b4d1e041d1a0f70
Joel Vargas <joelvargasapolinario@gmail.com>
2022-03-24 22:48:19 -0400

    email sent process

--- server/controllers/emailNotification.js
+++ server/controllers/emailNotification.js
@@ -1,1 +1,31 @@
 const User = require("../models/User");
+const nodemailer = require("nodemailer");
+
+const sendMail = () => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "AssetCalibrationSystemCOMP231",
+      pass: "SoftwareDevelopmentProject1",
+    },
+    tls: { rejectUnauthorized: false },
+  });
+
+  let mailOption = {
+    from: "AssetCalibrationSystemCOMP231@gmail.com",
+    to: "joelvargasapolinario@gmail.com",
+    subject: "Testing",
+    text: "First test",
+  };
+
+  transporter.sendMail(mailOption, (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+};
+module.exports = {
+  sendMail,
+};
