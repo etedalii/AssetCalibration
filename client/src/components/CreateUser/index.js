7067d4524a14bcec973b8aab318a49488b472c25
Joel Vargas <joelvargasapolinario@gmail.com>
2022-04-09 20:59:41 -0400

    Adding navbar

--- client/src/components/CreateUser/index.js
+++ client/src/components/CreateUser/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Navbar, FormControl, Form } from "react-bootstrap";
+import { Navbar, FormControl, Form, Container } from "react-bootstrap";
 const CreateUser = () => {
   return (
     <>
       <Navbar expand="lg" className="nav-color w-100 " variant="dark">
         <Form.Group
-          className={"d-flex justify-content-end position-relative  me-3"}
+          className={"ms-3 d-flex justify-content-end position-relative  me-3"}
         >
           <FormControl
             placeholder={"Search"}
@@ -16,8 +16,17 @@ const CreateUser = () => {
             className={`bi bi-search fa-search position-absolute  top-50 translate-middle s-2`}
           ></i>
         </Form.Group>
-        <button className="btn-lbl">logout</button>
+        <button className="me-3 btn-lbl">logout</button>
       </Navbar>
+      <div className="title-bar w-100 px-3">
+        <div className="d-flex justify-content-between w-100">
+          <span>maintenance supervisor dashboard</span>
+          <div>
+            <span className="me-3">Manage User</span>
+            <span>Add Asset</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
